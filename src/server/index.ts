ab71b2cd65ead82df94f4055a32a0a5ec3a1456e
kiminseob <kis6473@naver.com>
2021-01-01 22:37:35 +0900

    style: Change port number

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register';
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = 5000;
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 
